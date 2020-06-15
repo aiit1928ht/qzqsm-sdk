a7f8d65ca73f6c93bfd49eed4eedd8e388719eb4
Hiroshi Tokuyama in AIIT <a1928ht@aiit.ac.jp>
2020-06-16 02:17:30 +0900

    Del JSON Dependency on pom.xml and Update Weather.java

--- src/main/java/jp/ac/aiit/pbl/Weather.java
+++ src/main/java/jp/ac/aiit/pbl/Weather.java
@@ -1,64 +1,13 @@
 package jp.ac.aiit.pbl;
 
-import org.json.JSONObject;
-
 import java.time.LocalDateTime;
 
 public class Weather {
     public Weather(String qzqsm) {
-        this.rowData = new JSONObject();
-        this.rowData.put("pab", qzqsm.substring(0,9));
-        this.rowData.put("messageType", qzqsm.substring(8,14));
-        this.rowData.put("reportClassification", qzqsm.substring(14, 17));
-        this.rowData.put("disasterCategory", qzqsm.substring(17, 21));
-        this.rowData.put("reportTime", qzqsm.substring(21, 41));
-        this.rowData.put("infoType", qzqsm.substring(41, 43));
-        this.rowData.put("spare1", qzqsm.substring(43, 53));
-        this.rowData.put("warningState", qzqsm.substring(53, 56));
-        this.rowData.put("DisasterSubCategoryRegion1", qzqsm.substring(56, 61));
-        this.rowData.put("PrefecturalForecastRegionRegion1", qzqsm.substring(61, 80));
-        this.rowData.put("DisasterSubCategoryRegion2", qzqsm.substring(80, 85));
-        this.rowData.put("PrefecturalForecastRegionRegion2", qzqsm.substring(85, 104));
-        this.rowData.put("DisasterSubCategoryRegion3", qzqsm.substring(104, 109));
-        this.rowData.put("PrefecturalForecastRegionRegion3", qzqsm.substring(109, 128));
-        this.rowData.put("DisasterSubCategoryRegion4", qzqsm.substring(128, 133));
-        this.rowData.put("PrefecturalForecastRegionRegion4", qzqsm.substring(133, 152));
-        this.rowData.put("DisasterSubCategoryRegion5", qzqsm.substring(152, 157));
-        this.rowData.put("PrefecturalForecastRegionRegion5", qzqsm.substring(157, 176));
-        this.rowData.put("DisasterSubCategoryRegion6", qzqsm.substring(176, 181));
-        this.rowData.put("PrefecturalForecastRegionRegion6", qzqsm.substring(181, 200));
-        this.rowData.put("spare2", qzqsm.substring(200, 214));
-        this.rowData.put("versionNumber", qzqsm.substring(214, 220));
-        this.rowData.put("reserved", qzqsm.substring(220, 226));
-        this.rowData.put("crc", qzqsm.substring(226, 250));
-
-        this.decodedData = new JSONObject();
-        this.decodedData.put("pab", Integer.parseInt((String) this.rowData.get("pab"), 2));
-        this.decodedData.put("messageType", Integer.parseInt((String) this.rowData.get("messageType"), 2));
-        this.decodedData.put("reportClassification", Integer.parseInt((String) this.rowData.get("reportClassification"), 2));
-        this.decodedData.put("disasterCategory", Integer.parseInt((String) this.rowData.get("disasterCategory"), 2));
-        this.decodedData.put("reportTime", Integer.parseInt((String) this.rowData.get("reportTime"), 2));
-        this.decodedData.put("infoType", Integer.parseInt((String) this.rowData.get("infoType"), 2));
-        this.decodedData.put("warningState", Integer.parseInt((String) this.rowData.get("warningState"), 2));
-        this.decodedData.put("DisasterSubCategoryRegion1", Integer.parseInt((String) this.rowData.get("DisasterSubCategoryRegion1"), 2));
-        this.decodedData.put("PrefecturalForecastRegionRegion1", Integer.parseInt((String) this.rowData.get("PrefecturalForecastRegionRegion1"), 2));
-        this.decodedData.put("DisasterSubCategoryRegion2", Integer.parseInt((String) this.rowData.get("DisasterSubCategoryRegion2"), 2));
-        this.decodedData.put("PrefecturalForecastRegionRegion2", Integer.parseInt((String) this.rowData.get("PrefecturalForecastRegionRegion2"), 2));
-        this.decodedData.put("DisasterSubCategoryRegion3", Integer.parseInt((String) this.rowData.get("DisasterSubCategoryRegion3"), 2));
-        this.decodedData.put("PrefecturalForecastRegionRegion3", Integer.parseInt((String) this.rowData.get("PrefecturalForecastRegionRegion3"), 2));
-        this.decodedData.put("DisasterSubCategoryRegion4", Integer.parseInt((String) this.rowData.get("DisasterSubCategoryRegion4"), 2));
-        this.decodedData.put("PrefecturalForecastRegionRegion4", Integer.parseInt((String) this.rowData.get("PrefecturalForecastRegionRegion4"), 2));
-        this.decodedData.put("DisasterSubCategoryRegion5", Integer.parseInt((String) this.rowData.get("DisasterSubCategoryRegion5"), 2));
-        this.decodedData.put("PrefecturalForecastRegionRegion5", Integer.parseInt((String) this.rowData.get("PrefecturalForecastRegionRegion5"), 2));
-        this.decodedData.put("DisasterSubCategoryRegion6", Integer.parseInt((String) this.rowData.get("DisasterSubCategoryRegion6"), 2));
-        this.decodedData.put("PrefecturalForecastRegionRegion6", Integer.parseInt((String) this.rowData.get("PrefecturalForecastRegionRegion6"), 2));
-        
-        
+        this.qzqsm = qzqsm;
     }
 
-    private JSONObject rowData;
-    private JSONObject decodedData;
-
+    private String qzqsm;   // QZSSから受信した250bitの信号データ
     private String pab;
     private String messageType;
     private String reportClassification;
@@ -66,130 +15,168 @@ public class Weather {
     private LocalDateTime reportTime;
     private String infoType;
     private String warningState;
-    private String DisasterSubCategoryRegion1;
-    private String PrefecturalForecastRegionRegion1;
-    private String DisasterSubCategoryRegion2;
-    private String PrefecturalForecastRegionRegion2;
-    private String DisasterSubCategoryRegion3;
-    private String PrefecturalForecastRegionRegion3;
-    private String DisasterSubCategoryRegion4;
-    private String PrefecturalForecastRegionRegion4;
-    private String DisasterSubCategoryRegion5;
-    private String PrefecturalForecastRegionRegion5;
-    private String DisasterSubCategoryRegion6;
-    private String PrefecturalForecastRegionRegion6;
+    private String disasterSubCategoryRegion1;
+    private String prefecturalForecastRegionRegion1;
+    private String disasterSubCategoryRegion2;
+    private String prefecturalForecastRegionRegion2;
+    private String disasterSubCategoryRegion3;
+    private String prefecturalForecastRegionRegion3;
+    private String disasterSubCategoryRegion4;
+    private String prefecturalForecastRegionRegion4;
+    private String disasterSubCategoryRegion5;
+    private String prefecturalForecastRegionRegion5;
+    private String disasterSubCategoryRegion6;
+    private String prefecturalForecastRegionRegion6;
 
     public String getPab() {
         return pab;
+    }
+
+    public void setPab(String pab) {
+        this.pab = pab;
     }
 
     public String getMessageType() {
         return messageType;
     }
 
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
     public String getReportClassification() {
         return reportClassification;
+    }
+
+    public void setReportClassification(String reportClassification) {
+        this.reportClassification = reportClassification;
     }
 
     public String getDisasterCategory() {
         return disasterCategory;
     }
 
+    public void setDisasterCategory(String disasterCategory) {
+        this.disasterCategory = disasterCategory;
+    }
+
     public LocalDateTime getReportTime() {
         return reportTime;
+    }
+
+    public void setReportTime(LocalDateTime reportTime) {
+        this.reportTime = reportTime;
     }
 
     public String getInfoType() {
         return infoType;
     }
 
+    public void setInfoType(String infoType) {
+        this.infoType = infoType;
+    }
+
     public String getWarningState() {
         return warningState;
     }
 
+    public void setWarningState(String warningState) {
+        this.warningState = warningState;
+    }
+
     public String getDisasterSubCategoryRegion1() {
-        return DisasterSubCategoryRegion1;
+        return disasterSubCategoryRegion1;
+    }
+
+    public void setDisasterSubCategoryRegion1(String disasterSubCategoryRegion1) {
+        this.disasterSubCategoryRegion1 = disasterSubCategoryRegion1;
     }
 
     public String getPrefecturalForecastRegionRegion1() {
-        return PrefecturalForecastRegionRegion1;
+        return prefecturalForecastRegionRegion1;
+    }
+
+    public void setPrefecturalForecastRegionRegion1(String prefecturalForecastRegionRegion1) {
+        this.prefecturalForecastRegionRegion1 = prefecturalForecastRegionRegion1;
     }
 
     public String getDisasterSubCategoryRegion2() {
-        return DisasterSubCategoryRegion2;
+        return disasterSubCategoryRegion2;
+    }
+
+    public void setDisasterSubCategoryRegion2(String disasterSubCategoryRegion2) {
+        this.disasterSubCategoryRegion2 = disasterSubCategoryRegion2;
     }
 
     public String getPrefecturalForecastRegionRegion2() {
-        return PrefecturalForecastRegionRegion2;
+        return prefecturalForecastRegionRegion2;
+    }
+
+    public void setPrefecturalForecastRegionRegion2(String prefecturalForecastRegionRegion2) {
+        this.prefecturalForecastRegionRegion2 = prefecturalForecastRegionRegion2;
     }
 
     public String getDisasterSubCategoryRegion3() {
-        return DisasterSubCategoryRegion3;
+        return disasterSubCategoryRegion3;
+    }
+
+    public void setDisasterSubCategoryRegion3(String disasterSubCategoryRegion3) {
+        this.disasterSubCategoryRegion3 = disasterSubCategoryRegion3;
     }
 
     public String getPrefecturalForecastRegionRegion3() {
-        return PrefecturalForecastRegionRegion3;
+        return prefecturalForecastRegionRegion3;
+    }
+
+    public void setPrefecturalForecastRegionRegion3(String prefecturalForecastRegionRegion3) {
+        this.prefecturalForecastRegionRegion3 = prefecturalForecastRegionRegion3;
     }
 
     public String getDisasterSubCategoryRegion4() {
-        return DisasterSubCategoryRegion4;
+        return disasterSubCategoryRegion4;
+    }
+
+    public void setDisasterSubCategoryRegion4(String disasterSubCategoryRegion4) {
+        this.disasterSubCategoryRegion4 = disasterSubCategoryRegion4;
     }
 
     public String getPrefecturalForecastRegionRegion4() {
-        return PrefecturalForecastRegionRegion4;
+        return prefecturalForecastRegionRegion4;
+    }
+
+    public void setPrefecturalForecastRegionRegion4(String prefecturalForecastRegionRegion4) {
+        this.prefecturalForecastRegionRegion4 = prefecturalForecastRegionRegion4;
     }
 
     public String getDisasterSubCategoryRegion5() {
-        return DisasterSubCategoryRegion5;
+        return disasterSubCategoryRegion5;
+    }
+
+    public void setDisasterSubCategoryRegion5(String disasterSubCategoryRegion5) {
+        this.disasterSubCategoryRegion5 = disasterSubCategoryRegion5;
     }
 
     public String getPrefecturalForecastRegionRegion5() {
-        return PrefecturalForecastRegionRegion5;
+        return prefecturalForecastRegionRegion5;
+    }
+
+    public void setPrefecturalForecastRegionRegion5(String prefecturalForecastRegionRegion5) {
+        this.prefecturalForecastRegionRegion5 = prefecturalForecastRegionRegion5;
     }
 
     public String getDisasterSubCategoryRegion6() {
-        return DisasterSubCategoryRegion6;
+        return disasterSubCategoryRegion6;
+    }
+
+    public void setDisasterSubCategoryRegion6(String disasterSubCategoryRegion6) {
+        this.disasterSubCategoryRegion6 = disasterSubCategoryRegion6;
     }
 
     public String getPrefecturalForecastRegionRegion6() {
-        return PrefecturalForecastRegionRegion6;
+        return prefecturalForecastRegionRegion6;
     }
 
-    
-    enum Pab {
-    }
-
-    enum Mt {
-        TestMode,
-        MT43,
-        MT44
-    }
-
-    enum Rc {
-
-    }
-
-    enum Dc {
-
-    }
-
-    enum At {
-
-    }
-
-    enum It {
-
-    }
-
-    enum Ar {
-
-    }
-
-    enum Ww {
-
-    }
-
-    enum Pl {
-
+    public void setPrefecturalForecastRegionRegion6(String prefecturalForecastRegionRegion6) {
+        this.prefecturalForecastRegionRegion6 = prefecturalForecastRegionRegion6;
     }
 }
