8bdf8307b71097c98d8dbae31d941c87edf9b994
TakeshiHora <a1936th@aiit.ac.jp>
2020-06-24 01:08:53 +0900

    SeismicIntensity revised with Enum

--- src/main/java/jp/ac/aiit/pbl/SeismicIntensityCreator.java
+++
@@ -1,8 +0,0 @@
-package jp.ac.aiit.pbl;
-
-// creatorクラス
-public class SeismicIntensityCreator {
-    public SeismicIntensity seismicIntensityCreator(String dicimalNumber){
-        return new SeismicIntensity(dicimalNumber);
-    }
-}
