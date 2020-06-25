7ec11e2937a97b0f37f917888306a7b6b894a003
a1930yn <a1930yn@aiit.ac.jp>
2020-06-25 19:32:13 +0900

    Updated for compile errors.

--- src/main/java/jp/ac/aiit/pbl/Volcano.java
+++ src/main/java/jp/ac/aiit/pbl/Volcano.java
@@ -1,8 +1,68 @@
 package jp.ac.aiit.pbl;
-
-//import java.time.LocalDate;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Volcano {
+
+    private LocalDateTime activityTime;                     //Activity Time(Td)Day(D1)  Hour(H1) Minute(M1)
+    private VolcanoWarningCode warningCode;                 //Warning Code(Dw)
+    private VolcanoName volcanoName;                        //Volcano Name(Vo)
+    private VolcanoLocalGovernment localGovernmentRegion1;  //Local Government Region1
+    private VolcanoLocalGovernment localGovernmentRegion2;  //Local Government Region2
+    private VolcanoLocalGovernment localGovernmentRegion3;  //Local Government Region3
+    private VolcanoLocalGovernment localGovernmentRegion4;  //Local Government Region4
+    private VolcanoLocalGovernment localGovernmentRegion5;  //Local Government Region5
+
+    public LocalDateTime getActivityTime() {
+        return activityTime;
+    }
+    public void setActivityTime(LocalDateTime activityTime) {
+        this.activityTime = activityTime;
+    }
+    public VolcanoWarningCode getWarningCode() {
+        return warningCode;
+    }
+    public void setWarningCode(VolcanoWarningCode warningCode) {
+        this.warningCode = warningCode;
+    }
+    public VolcanoName getVolcanoName() {
+        return volcanoName;
+    }
+    public void setVolcanoName(VolcanoName volcanoName) {
+        this.volcanoName = volcanoName;
+    }
+    public VolcanoLocalGovernment getLocalGovernmentRegion1() {
+        return localGovernmentRegion1;
+    }
+    public void setLocalGovernmentRegion1(VolcanoLocalGovernment localGovernmentRegion1) {
+        this.localGovernmentRegion1 = localGovernmentRegion1;
+    }
+    public VolcanoLocalGovernment getLocalGovernmentRegion2() {
+        return localGovernmentRegion2;
+    }
+    public void setLocalGovernmentRegion2(VolcanoLocalGovernment localGovernmentRegion2) {
+        this.localGovernmentRegion2 = localGovernmentRegion2;
+    }
+    public VolcanoLocalGovernment getLocalGovernmentRegion3() {
+        return localGovernmentRegion3;
+    }
+    public void setLocalGovernmentRegion3(VolcanoLocalGovernment localGovernmentRegion3) {
+        this.localGovernmentRegion3 = localGovernmentRegion3;
+    }
+    public VolcanoLocalGovernment getLocalGovernmentRegion4() {
+        return localGovernmentRegion4;
+    }
+    public void setLocalGovernmentRegion4(VolcanoLocalGovernment localGovernmentRegion4) {
+        this.localGovernmentRegion4 = localGovernmentRegion4;
+    }
+    public VolcanoLocalGovernment getLocalGovernmentRegion5() {
+        return localGovernmentRegion5;
+    }
+    public void setLocalGovernmentRegion5(VolcanoLocalGovernment localGovernmentRegion5) {
+        this.localGovernmentRegion5 = localGovernmentRegion5;
+    }
+}
+
 //    private String preamble;                //Preample(PUB)
 //    private String messageType;             //Message Type(MT)
 //    private String reportClassification;    //Report Classification (rc) 2: Priority 3: Regular 7: Training/Test
@@ -13,90 +73,7 @@ public class Volcano {
 //    private String reportTimeMinute;        //Minute(AtMi)
 //    private String informationType;         //Information Type 0: Issue: 発表 1: Correction: 訂正 2: Cancellation: 取消
 //    private String Spare1;                  //Spare1 Fix to "0" for spare
-    private String activityTime;            //Activity Time(Td)Day(D1)  Hour(H1) Minute(M1)
-    private String warningCode;             //Warning Code(Dw)
-    private String volcanoName;             //Volcano Name(Vo)
-    private String localGovernmentRegion1; //Local Government Region1
-    private String localGovernmentRegion2; //Local Government Region2
-    private String localGovernmentRegion3; //Local Government Region3
-    private String localGovernmentRegion4; //Local Government Region4
-    private String localGovernmentRegion5; //Local Government Region5
-    private String spare2;                  //Spare2
-    private String version;                 //Version number(vn)
-    private String Reserved;
-    private String crc;                     //Cyclic Redundancy Check
-
-
-    public String getActivityTime() {
-        return activityTime;
-    }
-    public void setActivityTime(String activityTime) {
-        this.activityTime = activityTime;
-    }
-    public String getWarningCode() {
-        return warningCode;
-    }
-    public void setWarningCode(String warningCode) {
-        this.warningCode = warningCode;
-    }
-    public String getVolcanoName() {
-        return volcanoName;
-    }
-    public void setVolcanoName(String volcanoName) {
-        this.volcanoName = volcanoName;
-    }
-    public String getLocalGovernmentRegion1() {
-        return localGovernmentRegion1;
-    }
-    public void setLocalGovernmentRegion1(String localGovernmentRegion1) {
-        this.localGovernmentRegion1 = localGovernmentRegion1;
-    }
-    public String getLocalGovernmentRegion2() {
-        return localGovernmentRegion2;
-    }
-    public void setLocalGovernmentRegion2(String localGovernmentRegion2) {
-        this.localGovernmentRegion2 = localGovernmentRegion2;
-    }
-    public String getLocalGovernmentRegion3() {
-        return localGovernmentRegion3;
-    }
-    public void setLocalGovernmentRegion3(String localGovernmentRegion3) {
-        this.localGovernmentRegion3 = localGovernmentRegion3;
-    }
-    public String getLocalGovernmentRegion4() {
-        return localGovernmentRegion4;
-    }
-    public void setLocalGovernmentRegion4(String localGovernmentRegion4) {
-        this.localGovernmentRegion4 = localGovernmentRegion4;
-    }
-    public String getLocalGovernmentRegion5() {
-        return localGovernmentRegion5;
-    }
-    public void setLocalGovernmentRegion5(String localGovernmentRegion5) {
-        this.localGovernmentRegion5 = localGovernmentRegion5;
-    }
-    public String getSpare2() {
-        return spare2;
-    }
-    public void setSpare2(String spare2) {
-        this.spare2 = spare2;
-    }
-    public String getVersion() {
-        return version;
-    }
-    public void setVersion(String version) {
-        this.version = version;
-    }
-    public String getReserved() {
-        return Reserved;
-    }
-    public void setReserved(String reserved) {
-        Reserved = reserved;
-    }
-    public String getCrc() {
-        return crc;
-    }
-    public void setCrc(String crc) {
-        this.crc = crc;
-    }
-}
+//    private String spare2;                  //Spare2
+//    private String version;                 //Version number(vn)
+//    private String Reserved;
+//    private String crc;                     //Cyclic Redundancy Check
--- src/main/java/jp/ac/aiit/pbl/VolcanoParser.java
+++ src/main/java/jp/ac/aiit/pbl/VolcanoParser.java
@@ -7,26 +7,21 @@ public class VolcanoParser {
     public Volcano parse(String qzMessage){
         Volcano volcano = new Volcano();
         volcano.setActivityTime(toActivityTime(qzMessage.substring(53,69)));
-       // volcano.setWarningCode(WarningCode.getById(qzMessage.substring(69,76)));
-       // volcano.setVolcanoName(VolcanoName.getById(qzMessage.substring(76,88)));
-        //volcano.setLocalGovernmentRegion1(VolcanoLocalGovernment.getById(qzMessage.substring(88,111)));
-        //volcano.setLocalGovernmentRegion2(VolcanoLocalGovernment.getById(qzMessage.substring(111,134)));
-        //volcano.setLocalGovernmentRegion3(VolcanoLocalGovernment.getById(qzMessage.substring(134,157)));
-        //volcano.setLocalGovernmentRegion4(VolcanoLocalGovernment.getById(qzMessage.substring(157,180)));
-        //volcano.setLocalGovernmentRegion5(VolcanoLocalGovernment.getById(qzMessage.substring(180,203)));
-        volcano.setSpare2(qzMessage.substring(203,214));
-        volcano.setVersion(qzMessage.substring(214,220));
-        volcano.setReserved(qzMessage.substring(220,226));
-        volcano.setCrc(qzMessage.substring(226,250));
+        volcano.setWarningCode(VolcanoWarningCode.getById(Integer.parseInt(qzMessage.substring(69,76))));
+        volcano.setVolcanoName(VolcanoName.getById(Integer.parseInt(qzMessage.substring(76,88))));
+        volcano.setLocalGovernmentRegion1(VolcanoLocalGovernment.getById(Integer.parseInt(qzMessage.substring(88,111))));
+        volcano.setLocalGovernmentRegion2(VolcanoLocalGovernment.getById(Integer.parseInt(qzMessage.substring(111,134))));
+        volcano.setLocalGovernmentRegion3(VolcanoLocalGovernment.getById(Integer.parseInt(qzMessage.substring(134,157))));
+        volcano.setLocalGovernmentRegion4(VolcanoLocalGovernment.getById(Integer.parseInt(qzMessage.substring(157,180))));
+        volcano.setLocalGovernmentRegion5(VolcanoLocalGovernment.getById(Integer.parseInt(qzMessage.substring(180,203))));
         return volcano;
 
     }
 
-    private String toActivityTime(String message){
-        String day =  Integer.toString(Integer.parseInt(message.substring(0,4),2));
-        String hour =  Integer.toString(Integer.parseInt(message.substring(4,9),2));
-        String minute  = Integer.toString(Integer.parseInt(message.substring(9,15),2));
-
-        return day+"day"+hour+"hour"+minute+"minute";
+    private LocalDateTime toActivityTime(String message){
+            int day = Integer.parseInt(message.substring(0,4),2);
+            int hour = Integer.parseInt(message.substring(4,9),2);
+            int minute  = Integer.parseInt(message.substring(9,15),2);
+            return LocalDateTime.of(LocalDateTime.now().getYear(),LocalDateTime.now().getMonth(),day,hour,minute);
     }
 }
