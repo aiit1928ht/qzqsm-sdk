364543bc31971ea243b6847961142259b15f043d
hironotoru <a18z4th@icloud.com>
2020-07-03 00:59:15 +0900

    Typhoon
    課題[-103]対応

---
+++ src/main/java/jp/ac/aiit/pbl/ScaleCategory.java
@@ -0,0 +1,29 @@
+package jp.ac.aiit.pbl;
+
+import java.util.Arrays;
+
+public enum ScaleCategory {
+    scaleCategoryNone(0, "なし"),
+    scaleCategoryLargeScale(1, "大型"),
+    scaleCategorySuperScale(2, "超大型"),
+    scaleCategoryOthers(15,"その他の大きさ階級分類");
+
+    private int id;
+    private String scaleCategory;
+
+    ScaleCategory(int id, String scaleCategory){
+        this.id = id;
+        this.scaleCategory = scaleCategory;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public static ScaleCategory getById(int id) {
+        return Arrays.stream(ScaleCategory.values())
+                .filter(data -> data.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
---
+++ src/main/java/jp/ac/aiit/pbl/TypeOfReferenceTime.java
@@ -0,0 +1,28 @@
+package jp.ac.aiit.pbl;
+
+import java.util.Arrays;
+
+public enum TypeOfReferenceTime {
+    typeOfReferenceTimeLive(1, "実況"),
+    typeOfReferenceTimeEstimate(2, "推定"),
+    typeOfReferenceTimeForecast(3, "予報");
+
+    private int id;
+    private String typeOfReference;
+
+    TypeOfReferenceTime(int id, String typeOfReference){
+        this.id = id;
+        this.typeOfReference = typeOfReference;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public static TypeOfReferenceTime getById(int id) {
+        return Arrays.stream(TypeOfReferenceTime.values())
+                .filter(data -> data.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
