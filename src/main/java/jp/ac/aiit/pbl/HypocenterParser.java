3acd8b22c218101cbf809e6b5b0be089e0e5cd9c
TakeshiHora <a1936th@aiit.ac.jp>
2020-07-10 00:17:57 +0900

    revided Hypocenter

--- src/main/java/jp/ac/aiit/pbl/HypocenterParser.java
+++ src/main/java/jp/ac/aiit/pbl/HypocenterParser.java
@@ -38,6 +38,14 @@ public class HypocenterParser implements DisasterParser{
                 Integer.parseInt(qzMessage.substring(151, 157), 2),
                 Integer.parseInt(qzMessage.substring(157, 163), 2)));
         
+        
+        double latitude = parserLatlon(89, 59, 59);
+        double longitude = parserLatlon(179, 59, 59);
+        
+        assert latitude == 89.99972222222222;
+        assert longitude == 179.9997222222222;
+        
+        
         return hypocenter;
     }
     private LocalDateTime toOccurrenceTime(String message){
@@ -50,6 +58,4 @@ public class HypocenterParser implements DisasterParser{
     private double parserLatlon(int degree, int minute, int second) {
         return (double)degree + (double)minute/60.0d + (double)second/3600d;
     }
-    
-
 }
--- src/test/java/jp/ac/aiit/pbl/HypocenterParserTest.java
+++ src/test/java/jp/ac/aiit/pbl/HypocenterParserTest.java
@@ -15,18 +15,6 @@ public class HypocenterParserTest {
         
         Hypocenter hypocenter = hypocenterParser.parse("1100011010101101111100101100010100010110100000000000000110010100110011000110011101011000100011101111101011100101000000101111011001111011111011010110011111011111011");
         System.out.println(hypocenter);
-    }
-    @Test
-    public void parserLatlonTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        HypocenterParser hypocenterParser = new HypocenterParser();
-
-        double expected = 179.9997222222222;
-        Method method = HypocenterParser.class.getDeclaredMethod("parserLatlon",
-                int.class , int.class, int.class);
-        method.setAccessible(true);
-        double actual = (double)method.invoke(hypocenterParser, 179, 59, 59);
         
-        assertEquals(expected, actual, 0);
     }
-
 }
