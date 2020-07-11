8a1077b0133212e642f3f3d874f88e6db33f7f28
a18z4th@aiit.ac.jp <a18z4th@icloud.com>
2020-07-11 17:34:37 +0900

    [-111] NankaiTroughEarthquake
    NankaiTroughEarthquake class created
    and
    Typhoon and NankaiTroughEarthquake "Disaster" implements
    Typhoon and NankaiTroughEarthquake packaged
    and test passed

--- src/main/java/jp/ac/aiit/pbl/disaster/NankaiTroughEarthquake/NankaiTroughEarthquakeParser.java
+++ src/main/java/jp/ac/aiit/pbl/disaster/NankaiTroughEarthquake/NankaiTroughEarthquakeParser.java
@@ -6,30 +6,31 @@ public class NankaiTroughEarthquakeParser {
     public NankaiTroughEarthquake parse(String qzqms) {
         NankaiTroughEarthquake nankaiTroughEarthquake = new NankaiTroughEarthquake();
         PrefixParser preParser = new PrefixParser();
-        StringBuilder text = new StringBuilder();
 
         nankaiTroughEarthquake.setPrefix(preParser.parse(qzqms));
-
-        for (int bit = 57; bit < 201; bit += (8 + 8 + 8)) {
-            if (!qzqms.startsWith("00000000", bit)) {
-                text.append(new String(
-                        new byte[]{
-                                (byte) Integer.parseUnsignedInt(qzqms.substring(bit, bit + 8), 2),
-                                (byte) Integer.parseUnsignedInt(qzqms.substring(bit + 8, bit + 8 + 8), 2),
-                                (byte) Integer.parseUnsignedInt(qzqms.substring(bit + 8 + 8, bit + 8 + 8 + 8), 2)
-                        }
-                        )
-                );
-            } else {
-                break;
-            }
-        }
-
-        nankaiTroughEarthquake.setText(text.toString());
+        nankaiTroughEarthquake.setText(textTransducer((String) qzqms.subSequence(57,201)));
         nankaiTroughEarthquake.setPageNumber(Integer.valueOf((String) qzqms.subSequence(201,201 + 6),2));
         nankaiTroughEarthquake.setTotalPage(Integer.valueOf((String) qzqms.subSequence(207,207 + 6),2));
         // Speare2 1bit 213, 213 + 1
 
         return nankaiTroughEarthquake;
+    }
+
+    private String textTransducer(String qzqmsPart) {
+        StringBuilder text = new StringBuilder();
+        for (int bit = 0; bit < (6 * 8 * 3); bit += (8 + 8 + 8)) {
+            if (!qzqmsPart.startsWith("00000000", bit)) {
+                text.append(new String(
+                        new byte[]{
+                                (byte) Integer.parseUnsignedInt(qzqmsPart.substring(bit, bit + 8), 2),
+                                (byte) Integer.parseUnsignedInt(qzqmsPart.substring(bit + 8, bit + 8 + 8), 2),
+                                (byte) Integer.parseUnsignedInt(qzqmsPart.substring(bit + 8 + 8, bit + 8 + 8 + 8), 2)
+                        })
+                );
+            } else {
+                break;
+            }
+        }
+        return text.toString();
     }
 }
