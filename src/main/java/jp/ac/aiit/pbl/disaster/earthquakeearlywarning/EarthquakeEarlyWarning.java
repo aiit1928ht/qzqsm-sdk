6d3933e9da68adb3cf8589773028992858756183
Hiroshi Tokuyama in AIIT <a1928ht@aiit.ac.jp>
2020-07-19 17:28:28 +0900

    create new Java class of Earthquake-Early-Warning and a package of earthquakeearlywarning

---
+++ src/main/java/jp/ac/aiit/pbl/disaster/earthquakeearlywarning/EarthquakeEarlyWarning.java
@@ -0,0 +1,115 @@
+package jp.ac.aiit.pbl.disaster.earthquakeearlywarning;
+
+import jp.ac.aiit.pbl.*;
+import jp.ac.aiit.pbl.disaster.hypocenter.SeismicEpicenter;
+import java.time.LocalDateTime;
+
+public class EarthquakeEarlyWarning implements Disaster {
+    private Prefix prefix;
+    private Notification notification;
+    private LocalDateTime occurrenceTimeOfEarthquake;
+    private String depthOfSeismicEpicenter;
+    private String magnitude;
+    private SeismicEpicenter seismicEpicenter;
+    private SeismicIntensityLowerLimit seismicIntensityLowerLimit;
+    private SeismicIntensityUpperLimit seismicIntensityUpperLimit;
+    private List<ForecastRegionEarthquakeEarlyWarning> forecastRegionEarthquakeEarlyWarning;
+
+    public Prefix getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(Prefix prefix) {
+        this.prefix = prefix;
+    }
+
+    public Notification getNotification() {
+        return notification;
+    }
+
+    public void setNotification(Notification notification) {
+        this.notification = notification;
+    }
+
+    public LocalDateTime getOccurrenceTimeOfEarthquake() {
+        return occurrenceTimeOfEarthquake;
+    }
+
+    public void setOccurrenceTimeOfEarthquake(LocalDateTime occurrenceTimeOfEarthquake) {
+        this.occurrenceTimeOfEarthquake = occurrenceTimeOfEarthquake;
+    }
+
+    public String getDepthOfSeismicEpicenter() {
+        return depthOfSeismicEpicenter;
+    }
+
+    public void setDepthOfSeismicEpicenter(String depthOfSeismicEpicenter) {
+        this.depthOfSeismicEpicenter = depthOfSeismicEpicenter;
+    }
+
+    public String getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(String magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public SeismicEpicenter getSeismicEpicenter() {
+        return seismicEpicenter;
+    }
+
+    public void setSeismicEpicenter(SeismicEpicenter seismicEpicenter) {
+        this.seismicEpicenter = seismicEpicenter;
+    }
+
+    public SeismicIntensityLowerLimit getSeismicIntensityLowerLimit() {
+        return seismicIntensityLowerLimit;
+    }
+
+    public void setSeismicIntensityLowerLimit(SeismicIntensityLowerLimit seismicIntensityLowerLimit) {
+        this.seismicIntensityLowerLimit = seismicIntensityLowerLimit;
+    }
+
+    public SeismicIntensityUpperLimit getSeismicIntensityUpperLimit() {
+        return seismicIntensityUpperLimit;
+    }
+
+    public void setSeismicIntensityUpperLimit(SeismicIntensityUpperLimit seismicIntensityUpperLimit) {
+        this.seismicIntensityUpperLimit = seismicIntensityUpperLimit;
+    }
+
+    public List<ForecastRegionEarthquakeEarlyWarning> getForecastRegionEarthquakeEarlyWarning() {
+        return forecastRegionEarthquakeEarlyWarning;
+    }
+
+    public void setForecastRegionEarthquakeEarlyWarning(List<ForecastRegionEarthquakeEarlyWarning> forecastRegionEarthquakeEarlyWarning) {
+        this.forecastRegionEarthquakeEarlyWarning = forecastRegionEarthquakeEarlyWarning;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "    \"EarthquakeEarlyWarning\": {" +
+                "        \"prefix\": " + prefix + "," +
+                "        \"notification\": " + notification + "," +
+                "        \"occurrenceTimeOfEarthquake\": " + occurrenceTimeOfEarthquake + "," +
+                "        \"depthOfSeismicEpicenter\": " + depthOfSeismicEpicenter + "," +
+                "        \"seismicEpicenter\": " + seismicEpicenter + "," +
+                "        \"seismicIntensityLowerLimit\": " + seismicIntensityLowerLimit + "," +
+                "        \"seismicIntensityUpperLimit\": " + seismicIntensityUpperLimit + "," +
+                "        \"forecastRegionEarthquakeEarlyWarning\": " + forecastRegionEarthquakeEarlyWarning + "," +
+                "    }" +
+                "}";
+    }
+
+    @Override
+    public MessageType getMessageType() {
+        return prefix.getMessageType();
+    }
+
+    @Override
+    public DisasterCategory getDisasterCategory() {
+        return prefix.getDisasterCategory();
+    }
+}
